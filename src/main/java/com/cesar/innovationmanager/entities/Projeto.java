93e16bd7077358d9450f81c51d9dd987ca8c6d71
tn-junior <juju2004.tary@gmail.com>
2025-01-30 14:23:57 -0300

    feat: modifying to enum instead of string

--- src/main/java/com/cesar/innovationmanager/entities/Projeto.java
+++ src/main/java/com/cesar/innovationmanager/entities/Projeto.java
@@ -6,12 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Entity
+@Table(name = "projetos")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -23,26 +27,60 @@ public class Projeto {
     private Long id;
 
     @NotBlank(message = "O nome do projeto é obrigatório")
+    @Column(nullable = false, length = 150)
     private String nome;
 
     @Size(max = 500, message = "A descrição não pode ter mais que 500 caracteres")
     private String descricao;
 
-    @NotBlank(message = "O status do projeto é obrigatório")
-    private String status;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusProjeto status = StatusProjeto.RASCUNHO;
+
 
     @PastOrPresent(message = "A data de início deve ser no passado ou presente")
+    @Column(nullable = false)
     private LocalDateTime dataInicio;
 
     @FutureOrPresent(message = "A data de fim deve ser no futuro ou presente")
+    @Column(nullable = false)
     private LocalDateTime dataFim;
 
     @Positive(message = "O orçamento deve ser um valor positivo")
+    @Column(nullable = false, precision = 15, scale = 2)
     private BigDecimal orcamento;
 
     @NotBlank(message = "O líder do projeto é obrigatório")
+    @Column(nullable = false)
     private String liderProjeto;
 
-    @ElementCollection
-    private List<@NotBlank String> integrantes;
+    @ElementCollection(fetch = FetchType.EAGER) // carrega uma lista de integrantes junto ao projeto
+    @CollectionTable(name = "projeto_integrantes", joinColumns = @JoinColumn(name = "projeto_id"))
+    @Column(name = "integrante", nullable = false)
+    private Set<@NotBlank String> integrantes;
+
+    @CreationTimestamp // timestamp automático na criação
+    @Column(updatable = false)
+    private LocalDateTime criadoEm;
+
+    @UpdateTimestamp // Atualiza quando há modificações
+    private LocalDateTime atualizadoEm;
+
+    @PrePersist
+    protected void PrePersist() {
+        if (dataInicio == null) {
+            dataInicio = LocalDateTime.now();
+        }
+    }
+
+    @PreUpdate
+    protected void preUpdade(){
+        if (dataInicio.isAfter(dataFim)){
+            throw new IllegalStateException("A data de início não pode ser depois da data de fim");
+        }
+    }
+
+    public boolean isAprovado(){
+        return this.status == StatusProjeto.APROVADO;
+    }
 }
